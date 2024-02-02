1e437f00146b1f2ac292a2cde0d32ac644b7eb16
Sumit Paul <sumitp.scs@gmail.com>
2024-02-02 15:28:01 -0600

    inital code

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import Card from './components/Card'
+
+
+function App(props) {
+
+  let myObj = {
+    firstName:"Sumit",
+    lastName:"Paul"
+  }
+
+  
+  let [counter,setCounter] = useState(10)
+  function addValue(){  
+    if(counter < 20){
+      setCounter(counter+1);
+    }
+    
+  }
+
+  function decrease(){ 
+    if(counter > 0){setCounter(counter-1);} 
+    
+  }
+
+  return (
+    <>
+   
+   <h3>Counter : {counter} </h3> 
+   <button onClick={addValue}> Increase</button> <button onClick={decrease}>  Decrease</button>
+    <Card userName="Sumit" btnText="Click Me"/>
+    <Card />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+function Counter(){
+    let counter=10;
+    return(
+        <h1>Counter : {counter} </h1>
+
+    );    
+}
+
+export default Counter
+
+
